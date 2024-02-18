1605c684c9bb6c71c79a033b42a474da3281d057
LAMurakami <GitHub@LAMurakami.com>
2024-02-17 23:24:35 -0900

    Publish the EFS name for an aws region
    I could not use aws ec2 describe-instances to get the EFS value from the tags
    becasue I need credentials and all the secrets are on the EFS
    
    I decided the filesystem name for each region is not a secret.
    The EFS can only be accessed by an instance within the region.
    
    The github pages site is highly available but will become a prerequisite
    for mounting the EFS.  All this becasue I already have more than 16 scripts
    and don't want than many for each region where I will operate.

---
+++ aws-efs/REGION.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+TOKEN=$(curl -sX PUT "http://169.254.169.254/latest/api/token" \
+-H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+INSTANCE_ID=$(curl -sH "X-aws-ec2-metadata-token: $TOKEN" \
+http://169.254.169.254/latest/meta-data/instance-id)
+
+curl -sH "X-aws-ec2-metadata-token: $TOKEN" \
+http://169.254.169.254/latest/meta-data/placement/region
